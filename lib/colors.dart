371845f13499be23281759d7cda8feca09fcd538
Arpit Upadhyay <piyush9696upadhyay1234@gmail.com>
2025-10-02 17:09:32 +0530

    added colors.dart

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appBarColor = Color(0XFFf0f4f7);
+const Color bodyColor = Color(0XFFfcfefd);
+const Color iconBackground = Color(0XFF34353d);
+const Color bottomselectorColor = Color(0XFF222222);
